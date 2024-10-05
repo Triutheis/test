6fc7cca5fe35b1deb80cfe8ca9897ef5a0a7b494
TThai <163569473+Triutheis@users.noreply.github.com>
2024-10-06 00:39:11 +0700

    Update khoa.sh

--- khoa.sh
+++ khoa.sh
@@ -19,7 +19,7 @@ if [ "$INPUT_KEY" == "$KEY" ]; then
     # So sánh ngày hiện tại với ngày hết hạn
     if [[ "$CURRENT_DATE" < "$EXPIRATION_DATE" ]]; then
         echo "Truy cập thành công! Bạn có thể truy cập tệp tin $FILE."
-         && unzip v4.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x v4man && chmod a+x qoa && chmod a+x v4a;
+         unzip v4.zip && chmod a+x tun && chmod a+x psiphon-tunnel-core && chmod a+x qexit && chmod a+x wl && chmod a+x ver && chmod a+x v4man && chmod a+x qoa && chmod a+x v4a;
     else
         echo "Khóa đã hết hạn! Truy cập bị từ chối."
         chmod 000 "$FILE"  # Vô hiệu hóa quyền truy cập
